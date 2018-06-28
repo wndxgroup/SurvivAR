8a599c047ea46c09b034376fe412d3beab51a402
Derek Melchin <derek@wndx.com>
2018-06-27 18:03:24 -0600

    Improve view controller switching method

--- app/controllers/master_view_controller.rb
+++ app/controllers/master_view_controller.rb
@@ -8,16 +8,20 @@ class MasterViewController < UIViewController
   def viewDidLoad
     @start_here_controller = StartHereController.new
     self.addChildViewController(@start_here_controller)
-    prepare_start_here_screen
+    set_controller(@start_here_controller, from: nil)
   end
 
-  def prepare_start_here_screen
-    self.view.addSubview(@start_here_controller.view)
-    @start_here_controller.view.translatesAutoresizingMaskIntoConstraints = false
-    @start_here_controller.view.leftAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.leftAnchor).active = true
-    @start_here_controller.view.rightAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.rightAnchor).active = true
-    @start_here_controller.view.topAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.topAnchor).active = true
-    @start_here_controller.view.bottomAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.bottomAnchor).active = true
-    @start_here_controller.didMoveToParentViewController(self)
+  def set_controller(new_controller, from: old_controller)
+    if old_controller
+      old_controller.willMoveToParentViewController(nil)
+      old_controller.view.removeFromSuperview
+    end
+    self.view.addSubview(new_controller.view)
+    new_controller.view.translatesAutoresizingMaskIntoConstraints = false
+    new_controller.view.leftAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.leftAnchor).active = true
+    new_controller.view.rightAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.rightAnchor).active = true
+    new_controller.view.topAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.topAnchor).active = true
+    new_controller.view.bottomAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.bottomAnchor).active = true
+    new_controller.didMoveToParentViewController(self)
   end
 end
