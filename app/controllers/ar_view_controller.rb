fb9a0ffa909674d85756218a61e3d19092b9646d
Derek Melchin <derek@wndx.com>
2018-07-04 11:24:21 -0600

    Add vision to map button

--- app/controllers/ar_view_controller.rb
+++ app/controllers/ar_view_controller.rb
@@ -42,6 +42,19 @@ class ARViewController < UIViewController
     menu_icon_view = UIImageView.alloc.initWithImage(menu_icon)
     menu_icon_view.frame = [[0 ,0], [70, 70]]
     @menu_view.addSubview(menu_icon_view)
+
+    @map_view = UIView.new
+    view.addSubview(@map_view)
+    @map_view.translatesAutoresizingMaskIntoConstraints = false
+    @map_view.widthAnchor.constraintEqualToConstant(70).active = true
+    @map_view.heightAnchor.constraintEqualToConstant(70).active = true
+    @map_view.rightAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.rightAnchor).active = true
+    @map_view.bottomAnchor.constraintEqualToAnchor(view.safeAreaLayoutGuide.bottomAnchor).active = true
+
+    map_icon = UIImage.imageNamed('map-button')
+    map_icon_view = UIImageView.alloc.initWithImage(map_icon)
+    map_icon_view.frame = [[0, 0], [70, 70]]
+    @map_view.addSubview(map_icon_view)
   end
 
   def add_enemies
@@ -68,16 +81,31 @@ class ARViewController < UIViewController
   def touchesEnded(_, withEvent: event)
     if event.touchesForView(@menu_view)
       push_user_to_menu
+    elsif event.touchesForView(@map_view)
+      push_user_to_map
     else
       shoot
     end
   end
 
   def push_user_to_menu
+    pause_session
+    parentViewController.set_controller(parentViewController.menu_controller, from: self)
+  end
+
+  def push_user_to_map
+    pause_session
+    unless parentViewController.map_view_controller
+      parentViewController.start_map(self)
+    else
+      parentViewController.set_controller(parentViewController.map_view_controller, from: self)
+    end
+  end
+
+  def pause_session
     @scene_view.pointOfView.childNodes.each {|node| node.removeFromParentNode}
     @scene.rootNode.childNodes.each {|node| node.removeFromParentNode}
     @scene_view.session.pause
-    parentViewController.set_controller(parentViewController.menu_controller, from: self)
   end
 
   def shoot
