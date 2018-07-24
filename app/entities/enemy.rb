c08e6b73aad725e9e68cfdaf387f4ac1146db56d
Derek Melchin <derek@wndx.com>
2018-07-24 13:15:52 -0600

    Fix enemy speed bug

--- app/entities/enemy.rb
+++ app/entities/enemy.rb
@@ -6,7 +6,7 @@ class Enemy < GKEntity
   def add_components(entity_manager)
     addComponent(VisualComponent.new)
     move_component = MoveComponent.new
-    move_component.assign_properties(0.00000025, maxAcceleration: 5, radius: 1, entityManager: entity_manager)
+    move_component.assign_properties(0.00025, maxAcceleration: 5, radius: 1, entityManager: entity_manager)
     addComponent(move_component)
   end
 
@@ -17,11 +17,11 @@ class Enemy < GKEntity
     z = -z if rand < 0.5
     @node = self.componentForClass(VisualComponent).node
     @node.position = @position = SCNVector3Make(x, 0, z)
-    # @node.physicsBody = SCNPhysicsBody.bodyWithType(SCNPhysicsBodyTypeDynamic, shape: nil)#SCNPhysicsShape.shapeWithGeometry(SCNSphere.sphereWithRadius(1), options: nil))
-    # @node.physicsBody.categoryBitMask = 2
-    # @node.physicsBody.collisionBitMask = 1
-    # @node.physicsBody.contactTestBitMask = 1
-    # @node.physicsBody.affectedByGravity = false
+    @node.physicsBody = SCNPhysicsBody.bodyWithType(SCNPhysicsBodyTypeDynamic, shape: nil)#SCNPhysicsShape.shapeWithGeometry(SCNSphere.sphereWithRadius(1), options: nil))
+    @node.physicsBody.categoryBitMask = 2
+    @node.physicsBody.collisionBitMask = 1
+    @node.physicsBody.contactTestBitMask = 1
+    @node.physicsBody.affectedByGravity = false
     @node
   end
 end
