066f784bd0084065927ca3675969105cbc269fbc
Derek Melchin <derek@wndx.com>
2018-07-27 15:08:21 -0600

    Make AccountsListController a UITableViewController

--- app/controllers/accounts_list_controller.rb
+++ app/controllers/accounts_list_controller.rb
@@ -1,4 +1,4 @@
-class AccountsListController < UIViewController
+class AccountsListController < UITableViewController
   include SurvivalTime
 
   attr_accessor :add_icon_view
@@ -6,7 +6,7 @@ class AccountsListController < UIViewController
   def init
     super
     self.title = 'Accounts'
-    @table = UITableView.new
+    # @table = UITableView.new
     self
   end
 
@@ -15,18 +15,11 @@ class AccountsListController < UIViewController
     add_icon = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target: self, action: 'add_an_account')
     navigationItem.rightBarButtonItem = add_icon
     @player = Player.first
-    add_table
   end
 
-  def viewWillAppear(_)
-    @table.reloadData
-  end
-
-  def add_table
-    @table.frame = [[0, 0], [view.frame.size.width, view.frame.size.height]]
-    view.addSubview(@table)
-    @table.dataSource = @table.delegate = self
-  end
+  # def viewWillAppear(_)
+  #   @table.reloadData
+  # end
 
   def add_an_account
     navigationController.pushViewController(CreateAnAccountController.new, animated: true)
@@ -43,24 +36,13 @@ class AccountsListController < UIViewController
       cell
     end
     account = @player.sorted_accounts[indexPath.row]
-    additional_info = '💀' if !account.alive?
-    cell.textLabel.text = "#{account.username} #{additional_info}"
-    cell.detailTextLabel.text = "#{account.kills} kills in #{survival_time(account)}"
+    cell.textLabel.text = account.username
     cell
   end
 
   def tableView(_, didSelectRowAtIndexPath: indexPath)
-    @player.sorted_accounts[@player.current_account].start_time = nil if @player.current_account
-    @player.current_account = indexPath.row
-    cdq.save
-    if @player.sorted_accounts[@player.current_account].alive?
-      if navigationController.viewControllers.count == 1
-        navigationController.setViewControllers([MenuController.new], animated: true)
-      else
-        navigationController.popViewControllerAnimated(true)
-      end
-    else
-      navigationController.pushViewController(DeathController.new, animated: true)
-    end
+    controller = MyAccountController.new
+    controller.set_account(indexPath.row)
+    navigationController.pushViewController(controller, animated: true)
   end
 end
