dea5e3cacaf14f236537995abaf5b334e3152acf
Derek Melchin <derek@wndx.com>
2018-08-24 13:55:40 -0600

    Simplify Bullet entity

--- app/components/bullet_agent.rb
+++ app/components/bullet_agent.rb
@@ -1,8 +1,23 @@
 class BulletAgent < GKAgent3D
 
+  attr_accessor :node
+
+  def init
+    super
+    geometry = SCNSphere.sphereWithRadius(0.1)
+    material = SCNMaterial.material
+    material.diffuse.contents = UIColor.whiteColor
+    geometry.materials = [material]
+    @node = SCNNode.nodeWithGeometry(geometry)
+    @node.physicsBody = SCNPhysicsBody.bodyWithType(SCNPhysicsBodyTypeDynamic, shape: nil)
+    @node.physicsBody.categoryBitMask = 1
+    @node.physicsBody.collisionBitMask = 2
+    @node.physicsBody.contactTestBitMask = 2
+    self
+  end
+
   def updateWithDeltaTime(seconds)
     super
-    @node = entity.componentForClass(BulletComponent).node
     delete_entity if @node.presentationNode.position.y < -10
   end
 
--- app/entities/bullet.rb
+++ app/entities/bullet.rb
@@ -3,7 +3,6 @@ class Bullet < GKEntity
 
   def init
     super
-    addComponent(BulletComponent.new)
     addComponent(@agent = BulletAgent.new)
     self
   end
@@ -13,16 +12,8 @@ class Bullet < GKEntity
   end
 
   def set_firing_location(position)
-    @node = self.componentForClass(BulletComponent).node
+    @node = @agent.node
     @node.position = position
-    @node.physicsBody = SCNPhysicsBody.bodyWithType(SCNPhysicsBodyTypeDynamic, shape: nil)
-    @node.physicsBody.categoryBitMask = 1
-    @node.physicsBody.collisionBitMask = 2
-    @node.physicsBody.contactTestBitMask = 2
     @node
-  end
-
-  def set_trajectory(trajectory, entity_manager: entity_manager)
-    @agent.set_trajectory(trajectory, entity_manager: entity_manager)
   end
 end
